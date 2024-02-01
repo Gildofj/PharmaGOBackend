70bc3b02e0267e8026931f3681ec9abd952991f2
Gildo Junior <1gildojunior@gmail.com>
2024-02-01 00:38:35 -0300

    fix concepts

--- PharmaGOBackend.Domain/Common/Errors/Errors.Pharmacy.cs
+++ PharmaGOBackend.Domain/Common/Errors/Errors.Pharmacy.cs
@@ -6,7 +6,7 @@ public static partial class Errors
 {
     public static class Pharmacy
     {
-        public static Error NameNotInformed => Error.Conflict(code: "Pharmacy.NameNotInformed ", description: "Name not informed.");
-        public static Error CnpjNotInformed => Error.Conflict(code: "Pharmacy.CnpjNotInformed", description: "CNPJ not informed.");
+        public static Error NameNotInformed => Error.Validation(code: "Pharmacy.NameNotInformed ", description: "Name not informed.");
+        public static Error CnpjNotInformed => Error.Validation(code: "Pharmacy.CnpjNotInformed", description: "CNPJ not informed.");
     }
 }
--- PharmaGOBackend.Domain/Common/Errors/Errors.Product.cs
+++ PharmaGOBackend.Domain/Common/Errors/Errors.Product.cs
@@ -5,9 +5,9 @@ public static partial class Errors
 {
     public static class Product
     {
-        public static Error NameNotInformed => Error.Conflict(code: "Product.NameNotInformed", description: "Name not informed.");
-        public static Error AmountNotInformed => Error.Conflict(code: "Product.AmountNotInformed", description: "Amount not informed.");
+        public static Error NameNotInformed => Error.Validation(code: "Product.NameNotInformed", description: "Name not informed.");
+        public static Error AmountNotInformed => Error.Validation(code: "Product.AmountNotInformed", description: "Amount not informed.");
         public static Error DescriptionExceededMaxLength => Error.Conflict(code: "Product.DescriptionExceededMaxLength", description: "Exceeded maximum description length. (Max: 300)");
-        public static Error PharmacyIdNotInformed => Error.Conflict(code: "Product.PharmacyIdNotInformed", description: "PharmacyId not informed.");
+        public static Error PharmacyIdNotInformed => Error.Validation(code: "Product.PharmacyIdNotInformed", description: "PharmacyId not informed.");
     }
 }
