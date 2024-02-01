33be88b5fc2a4b73a4bf2995ce9b35868c2c88c9
Gildo Junior <1gildojunior@gmail.com>
2024-02-01 00:44:38 -0300

    performace tips

--- PharmaGOBackend.Api/Controllers/ApiController.cs
+++ PharmaGOBackend.Api/Controllers/ApiController.cs
@@ -23,7 +23,7 @@ public abstract class ApiController : ControllerBase
         return Problem(errors[0]);
     }
 
-    private IActionResult Problem(Error error)
+    private ObjectResult Problem(Error error)
     {
         var statusCode = error.Type switch
         {
@@ -36,7 +36,7 @@ public abstract class ApiController : ControllerBase
         return Problem(statusCode: statusCode, title: error.Description);
     }
 
-    private IActionResult ValidationProblem(List<Error> errors)
+    private ActionResult ValidationProblem(List<Error> errors)
     {
         var modelStateDictionary = new ModelStateDictionary();
 
